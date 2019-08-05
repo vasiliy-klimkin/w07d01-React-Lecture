b5d96deb46698258ad0dbd6a22f025d11ff1210b
Vasiliy Klimkin <vasily.klimkin@gmail.com>
2019-08-05 13:53:10 -0400

    :lipstick: Added one more story.

--- todo-list-demo/stories/ListContainer.stories.js
+++ todo-list-demo/stories/ListContainer.stories.js
@@ -4,11 +4,8 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import ListItem from '../src/components/ListItem';
+import ListContainer from '../src/components/ListContainer';
 
-storiesOf('List Item', module)
-  .add('Simple', () => <ListItem>Simple List Item</ListItem>)
-  .add('With Emoji', () => (
-    <ListItem>👍</ListItem>
-  ))
-  .add('hightligted', () => <ListItem highlight>hightligted</ListItem>)
+storiesOf('List Container', module)
+  .add('List empty', () => <ListContainer items={[]} />)
+  .add('List with 3 Items', () => <ListContainer items={['Buy Milk', 'Learn React', 'Eat Food']}/>)
